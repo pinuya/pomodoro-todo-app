905857afff78966e0c3edd7b4df8668f132ed6c5
Tifany <tifsnunes@gmail.com>
2025-03-04 11:07:42 -0300

    add: zod lib and actions

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,7 @@
-import type { MetaFunction } from "@remix-run/node";
-import { ListChecks, Minus, PictureInPicture2, Plus, X } from "lucide-react";
+import { useState } from "react";
+import { Form, useActionData } from "@remix-run/react";
+import { ActionFunctionArgs, MetaFunction } from "@remix-run/node";
+import { ListChecks, Minus, PictureInPicture2, X, Plus } from "lucide-react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,7 +10,23 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const formData = await request.formData();
+  const todoItem = formData.get("todoItem");
+
+  return { todoItem };
+};
+
 export default function Index() {
+  const actionData = useActionData<typeof action>();
+  const [todos, setTodos] = useState<string[]>([]);
+
+  const handleAddTodo = () => {
+    if (actionData?.todoItem) {
+      setTodos((prevTodos) => [...prevTodos, actionData.todoItem as string]);
+    }
+  };
+
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="bg-100 rounded-2xl w-full max-w-2xl mx-auto border-2 shadow-lg">
@@ -19,15 +37,15 @@ export default function Index() {
           </div>
 
           <div className="flex items-center gap-1 sm:gap-3">
-            <div className="p-1 cursor-pointer flex items-center justify-center bg-300">
+            <div className="p-1 flex items-center justify-center bg-300">
               <Minus className="h-4 w-4 sm:h-5 sm:w-5" />
             </div>
 
-            <div className="p-1 cursor-pointer flex items-center justify-center bg-300">
+            <div className="p-1 flex items-center justify-center bg-300">
               <PictureInPicture2 className="h-4 w-4 sm:h-5 sm:w-5" />
             </div>
 
-            <div className="p-1 cursor-pointer flex items-center justify-center bg-300">
+            <div className="p-1 flex items-center justify-center bg-300">
               <X className="h-4 w-4 sm:h-5 sm:w-5" />
             </div>
           </div>
@@ -39,23 +57,34 @@ export default function Index() {
               Adicione um item a sua lista
             </span>
 
-            <div className="bg-300/60 border-2 rounded-2xl flex items-center p-2">
+            <Form
+              method="post"
+              className="bg-300/60 border-2 rounded-2xl flex items-center p-2"
+              onSubmit={handleAddTodo}
+            >
               <input
                 type="text"
                 placeholder="Digite aqui..."
                 className="flex-1 bg-transparent outline-none px-0 border-0 appearance-none focus:ring-0"
                 style={{ backgroundColor: "transparent", boxShadow: "none" }}
               />
-              <Plus className="ml-2 cursor-pointer" />
-            </div>
+              <button type="submit">
+                <Plus className="ml-2 cursor-pointer" />
+              </button>
+            </Form>
           </div>
 
           <div className="bg-400 w-full h-96 rounded-b-2xl flex justify-center items-center p-4">
             <div className="w-96">
               <div className="overflow-y-auto max-h-64 custom-scrollbar pr-2">
-                <div className="bg-100 border-2 p-4 w-full mb-4 line-clamp-1">
-                  teste
-                </div>
+                {todos.map((todo, index) => (
+                  <div
+                    key={index}
+                    className="bg-100 border-2 p-4 w-full mb-4 line-clamp-1"
+                  >
+                    {todo}
+                  </div>
+                ))}
               </div>
             </div>
           </div>
